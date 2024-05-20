7845a88b49717ea2c4cde14a8a587875a1cb814e
Diego Melo <diego_melo@outlook.es>
2024-05-19 23:51:17 -0600

    Dashboard de academia

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,9 +9,9 @@ if(ctx) {
   var myPieChart = new Chart(ctx, {
     type: 'pie',
     data: {
-      labels: ["Blue", "Red", "Yellow", "Green"],
+      labels: ["Sistemas", "Telecomunicaciones", "Arquitectura", "Actuaría"],
       datasets: [{
-        data: [12.21, 15.58, 11.25, 8.32],
+        data: [12, 15, 11, 8],
         backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
       }],
     },
