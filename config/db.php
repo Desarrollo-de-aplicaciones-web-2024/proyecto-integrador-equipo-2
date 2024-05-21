7fed1a1bda08ef7c165f59cca2495d773ed42228
javierjararenteria@gmail.com <202160023@ucc.mx>
2024-05-21 07:59:49 -0600

    no subir las credenciales de aws

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect('sistema-practicas.cluster-c7y2igeuk57s.us-east-2.rds.amazonaws.com', 'koda', '134acbe03e045066df8ca596126f14a5', 'sistema_practicas',3306);
+$conexion = mysqli_connect('localhost', 'root', '', 'sistema_practicas');
 
 if ($conexion === false) { //¿error?
     exit('Error en la conexión con la bd');
